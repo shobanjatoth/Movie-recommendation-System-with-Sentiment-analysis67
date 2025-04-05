f6bf8a5a96d4badefca4c7034d231c2548134890
shobanjatoth <146610402+shobanjatoth@users.noreply.github.com>
2025-04-05 22:05:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -219,6 +219,6 @@ def recommend():
                            suggestions=suggestions)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ['PORT'])
     app.run(host='0.0.0.0', port=port)
     
